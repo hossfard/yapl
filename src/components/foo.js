ba81d1c246faa02c4863754486d08ea00d0acd20
Hossein Fadaifard <fadaifard@gmail.com>
2021-07-24 19:34:45 -0500

    Add HTickGenreator class

--- src/components/foo.js
+++ src/components/foo.js
@@ -121,6 +121,41 @@ class VTickGenerator{
 }
 
 
+// eslint-disable-next-line no-unused-vars
+class HTickGenerator{
+   constructor(tickLength){
+      this.tickLength = tickLength;
+   }
+
+   // Vertically up
+   generateTick(canvasValue){
+      return new Konva.Line({
+         points: [
+            0, canvasValue,
+            0+this.tickLength, canvasValue
+         ],
+         stroke: 'black',
+         strokeWidth: 1
+      });
+   }
+
+   generateLabel(value, canvasValue){
+      let text = new Konva.Text({
+         x: 0,
+         y: canvasValue,
+         fontFamily: 'Calibri',
+         fontSize: 14,
+         text: value.toLocaleString(
+            undefined, {minimumFractionDigits: 2}
+         ),
+         fill: 'black',
+      });
+      text.y(canvasValue - text.width());
+      return text;
+   }
+}
+
+
 export class AbstractRenderDelegate{
    constructor(tickHeight, tickCount, tickGenerator){
       this.tickHeight = tickHeight || 5;
