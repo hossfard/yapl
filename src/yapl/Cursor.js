7cba5b11159163cf7dceb95e32d9263847ee1acd
Hossein Fadaifard <fadaifard@gmail.com>
2021-08-22 23:54:06 -0500

    Add docs for Cursor

--- src/yapl/Cursor.js
+++ src/yapl/Cursor.js
@@ -2,7 +2,16 @@
 
 
 
+/** Convenience methods to interact with cursor
+ */
 export class Cursor{
+
+   /** Set cursor type
+    *
+    * @see [MDN web docs]{@link https://developer.mozilla.org/en-US/docs/Web/CSS/cursor}
+    *
+    * @param {string} type cursor type
+    */
    static set(type){
       document.body.style.cursor = type;
    }
